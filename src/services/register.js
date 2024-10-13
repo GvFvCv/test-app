7917c330eb897c9e26d4c4375b438ba6773eadac
Alejandro Bovet <102919972+AlejoBovet@users.noreply.github.com>
2024-10-13 19:43:33 -0300

    Update register.js
    - Se agrega guardado de respuesta del backend en localstorage

--- src/services/register.js
+++ src/services/register.js
@@ -1,16 +1,25 @@
 export const registerapp = async (formData) => {
+  try {
     const response = await fetch('http://127.0.0.1:8000/app/register/', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',  // Necesario si envías JSON
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(formData),
     });
-  
+
     if (!response.ok) {
       throw new Error('Error en el registro');
     }
-  
-    return response.json();
-  };
-  
+
+    const data = await response.json();
+
+    // Almacenar la respuesta en localStorage
+    localStorage.setItem('registerResponse', JSON.stringify(data));
+
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
